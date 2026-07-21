7c1f867271eebfea9399602a235ff8b0591d84b7
Kayo Gomes <theonascimento612@gmail.com>
2026-07-21 14:45:29 -0300

    Refactor game logic to use Chart.js for graphing

--- script.js
+++ script.js
@@ -1,111 +1,30 @@
-const canvas = document.getElementById("game");
-const ctx = canvas.getContext("2d");
+const ctx = document.getElementById("grafico");
 
-const box = 20;
+new Chart(ctx,{
+type:"line",
 
-let snake = [
-    {x:10*box, y:10*box}
-];
+data:{
 
-let direction = "RIGHT";
+labels:["Jan","Fev","Mar","Abr","Mai","Jun"],
 
-let food = {
-    x: Math.floor(Math.random()*20)*box,
-    y: Math.floor(Math.random()*20)*box
-};
+datasets:[{
 
-let score = 0;
+label:"Vendas",
 
-document.addEventListener("keydown", update);
+data:[1200,1900,1500,2500,3200,4200],
 
-function update(event){
+borderWidth:3,
 
-    if(event.key=="ArrowLeft" && direction!="RIGHT")
-        direction="LEFT";
+fill:false
 
-    if(event.key=="ArrowUp" && direction!="DOWN")
-        direction="UP";
+}]
 
-    if(event.key=="ArrowRight" && direction!="LEFT")
-        direction="RIGHT";
+},
 
-    if(event.key=="ArrowDown" && direction!="UP")
-        direction="DOWN";
-}
+options:{
 
-function draw(){
-
-    ctx.fillStyle="black";
-    ctx.fillRect(0,0,400,400);
-
-    for(let i=0;i<snake.length;i++){
-
-        ctx.fillStyle=i==0 ? "lime":"green";
-        ctx.fillRect(snake[i].x,snake[i].y,box,box);
-    }
-
-    ctx.fillStyle="red";
-    ctx.fillRect(food.x,food.y,box,box);
-
-    let snakeX=snake[0].x;
-    let snakeY=snake[0].y;
-
-    if(direction=="LEFT") snakeX-=box;
-    if(direction=="UP") snakeY-=box;
-    if(direction=="RIGHT") snakeX+=box;
-    if(direction=="DOWN") snakeY+=box;
-
-    if(snakeX==food.x && snakeY==food.y){
-
-        score++;
-        document.getElementById("score").innerHTML=score;
-
-        food={
-            x:Math.floor(Math.random()*20)*box,
-            y:Math.floor(Math.random()*20)*box
-        };
-
-    }else{
-
-        snake.pop();
-
-    }
-
-    const newHead={
-        x:snakeX,
-        y:snakeY
-    };
-
-    if(
-        snakeX<0 ||
-        snakeY<0 ||
-        snakeX>=400 ||
-        snakeY>=400 ||
-        collision(newHead,snake)
-    ){
-
-        clearInterval(game);
-        alert("Game Over! Pontuação: "+score);
-    }
-
-    snake.unshift(newHead);
+responsive:true
 
 }
 
-function collision(head,array){
-
-    for(let i=0;i<array.length;i++){
-
-        if(head.x==array[i].x && head.y==array[i].y){
-
-            return true;
-
-        }
-
-    }
-
-    return false;
-
-}
-
-const game=setInterval(draw,120);
+});
